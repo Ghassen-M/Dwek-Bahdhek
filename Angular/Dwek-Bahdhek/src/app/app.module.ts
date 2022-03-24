317d479fb0385034ebee1ea10cdc75818e1d3227
Ghassen-M <mastourighassen@gmail.com>
2022-03-24 19:25:27 +0100

    created login component with its full frontend functionalities (still needs a bit of design modifications once integrated with the other components)

--- Angular/Dwek-Bahdhek/src/app/app.module.ts
+++ Angular/Dwek-Bahdhek/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/Dwek-Bahdhek/src/app/login/login.component.ts
+++ Angular/Dwek-Bahdhek/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  isInscri = false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
+
+  inscri(): void {
+    this.isInscri = true;
+  }
+  connecte(): void {
+    this.isInscri = !this.inscri;
+  }
 }
