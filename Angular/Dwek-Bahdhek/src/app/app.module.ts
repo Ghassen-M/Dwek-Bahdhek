38b15a049223ba9bbd51f2c822ecb2a682b035b8
Ghassen-M <mastourighassen@gmail.com>
2022-04-11 15:44:08 +0100

    .

--- Angular/Dwek-Bahdhek/src/app/app.module.ts
+++ Angular/Dwek-Bahdhek/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
