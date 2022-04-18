ebb81106b01f6648d3333defac398e4614ebb160
Ghassen Mastouri <mastourighassen@gmail.com>
2022-04-18 23:28:31 +0100

    .

--- Angular/Dwek-Bahdhek/src/app/signinclient/signinclient.component.ts
+++ Angular/Dwek-Bahdhek/src/app/signinclient/signinclient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signinclient',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signinclient.component.scss']
 })
 export class SigninclientComponent implements OnInit {
-
-  constructor() { }
+  title = "créer un compte client"
+  sexe = ["homme", "femme"];
+  data = { nom: "", prenom: "", email: "", mdp: "", dn: "", sexe: "" };
+  client = new FormGroup({
+    nom: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z]*$")]),
+    prenom: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z]*$")]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    mdp: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    dn: new FormControl('', [Validators.required]),
+    sexe: new FormControl('', [Validators.required]),
+    tel: new FormControl('', [Validators.required]),
+    cin: new FormControl('', [Validators.required])
+  })
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+
+  }
+  get f() {
+    return this.client.controls;
   }
 
+  onSubmit() {
+
+    this.api.ajoutClient(this.client.value).subscribe(data => console.log(data));
+    alert('validé');
+
+  }
 }
+
--- Angular/Dwek-Bahdhek/src/app/signinpharmacien/signinpharmacien.component.ts
+++ Angular/Dwek-Bahdhek/src/app/signinpharmacien/signinpharmacien.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signinpharmacien',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninpharmacienComponent implements OnInit {
 
-  constructor() { }
+  title = "créer un compte pharmacien"
+  sexe = ["homme", "femme"];
+  data = { nom: "", prenom: "", cin: "", email: "", mdp: "", dn: "", sexe: "", tel: "", lr: "" };
+
+  pharmacien = new FormGroup({
+    nom: new FormControl(this.data.nom, [Validators.required, Validators.pattern("[A-Za-z]*")]),
+    prenom: new FormControl(this.data.prenom, [Validators.required, Validators.pattern("[A-Za-z]*")]),
+    cin: new FormControl(this.data.cin, [Validators.required, Validators.pattern("^[0-9]{8}$")]),
+    email: new FormControl(this.data.email, [Validators.required, Validators.email]),
+    mdp: new FormControl(this.data.mdp, [Validators.required, Validators.minLength(6)]),
+    dn: new FormControl(this.data.dn, [Validators.required]),
+    sexe: new FormControl(this.data.sexe, [Validators.required]),
+    tel: new FormControl(this.data.tel, [Validators.required, Validators.pattern("^[0-9]{8}$")]),
+    lr: new FormControl(this.data.lr, [Validators.required]),
+
+  })
 
   ngOnInit(): void {
+
+  }
+  get f() {
+    return this.pharmacien.controls;
   }
 
+  onSubmit() {
+    if (this.pharmacien.valid) {
+      alert('valide ');
+      console.table(this.pharmacien.value);
+    }
+  }
 }
